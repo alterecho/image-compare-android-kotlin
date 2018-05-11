30718af62c4c2478a4890a2796afbb828c3ea24f
vj <alterechoxii@gmail.com>
2018-05-11 19:07:37 +0530

    added ImageInspectorView

--- app/src/main/java/vjc/com/imagecompare/ImageInspectorView.kt
+++ app/src/main/java/vjc/com/imagecompare/ImageInspectorView.kt
@@ -1,8 +1,10 @@
 package vjc.com.imagecompare
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.widget.FrameLayout
 import android.widget.ImageView
 
@@ -16,10 +18,38 @@ class ImageInspectorView : FrameLayout {
 
     }
 
-    fun init() {
-        setBackgroundColor(Color.BLUE)
+    var bitmap: Bitmap? = null
+    set(value) {
+        _imageView.setImageBitmap(field)
+        _imageView.layoutParams.width = 100
+        _imageView.layoutParams.height = 100
+        field?.let {
+            _imageView.layoutParams.width = it.width
+            _imageView.layoutParams.height = it.height
+        }
+        _imageView.requestLayout()
     }
 
 
     private val _imageView: ImageView = ImageView(this.context)
+
+    private fun init() {
+        setBackgroundColor(Color.CYAN)
+        this.addView(_imageView)
+        _imageView.setBackgroundColor(Color.RED)
+        this.bitmap = null
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> println("down")
+        }
+
+        return false
+    }
+
+
+
 }
