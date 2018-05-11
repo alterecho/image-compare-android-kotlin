13977dfb227fce5000c421bdeb7d10b09dc12641
vj <alterechoxii@gmail.com>
2018-05-11 22:33:57 +0530

    touch offset

--- app/src/main/java/vjc/com/imagecompare/ImageInspectorView.kt
+++ app/src/main/java/vjc/com/imagecompare/ImageInspectorView.kt
@@ -8,6 +8,10 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.widget.FrameLayout
 import vjc.com.imagecompare.ImageView
+
+fun PointF.byAdding(point: PointF): PointF = PointF(this.x + point.x, this.y + point.y)
+fun PointF.bySubtracting(point: PointF): PointF = PointF(this.x - point.x, this.y - point.y)
+
 class ImageInspectorView : FrameLayout {
 
     constructor(ctx: Context) : super(ctx){
@@ -24,22 +28,17 @@ class ImageInspectorView : FrameLayout {
     }
 
 
-    private val _imageView: ImageView = vjc.com.imagecompare.ImageView(this.context)
 
-    private fun init() {
-        setBackgroundColor(Color.CYAN)
-        this.addView(_imageView)
-        _imageView.setBackgroundColor(Color.RED)
-        this.bitmap = null
-
-    }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         event?.let {
             val touchPoint = PointF(it.x, it.y);
+
             when (it.action) {
                 MotionEvent.ACTION_DOWN -> {
                     println("down")
+                    _touchPoint_began = touchPoint
+                    _touchPoint_offset = touchPoint.bySubtracting(_imageView.position)
                     return true
                 }
 
@@ -48,7 +47,7 @@ class ImageInspectorView : FrameLayout {
                 }
 
                 MotionEvent.ACTION_MOVE -> {
-                    _imageView.position = touchPoint
+                    _imageView.position = touchPoint.bySubtracting(_touchPoint_offset)
                 }
 
                 MotionEvent.ACTION_POINTER_UP -> {
@@ -56,7 +55,7 @@ class ImageInspectorView : FrameLayout {
                 }
 
                 MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-
+                    _touchPoint_offset = PointF()
                 }
             }
         }
@@ -66,6 +65,19 @@ class ImageInspectorView : FrameLayout {
         return false
     }
 
+
+    private val _imageView: ImageView = vjc.com.imagecompare.ImageView(this.context)
+    private var _touchPoint_began = PointF(0.0f, 0.0f)
+    /** difference between where the the touch began and where _imageView position (center) */
+    private var _touchPoint_offset = PointF()
+
+    private fun init() {
+        setBackgroundColor(Color.CYAN)
+        this.addView(_imageView)
+        _imageView.setBackgroundColor(Color.RED)
+        this.bitmap = null
+
+    }
 
 
 }
--- app/src/main/java/vjc/com/imagecompare/ImageView.kt
+++ app/src/main/java/vjc/com/imagecompare/ImageView.kt
@@ -9,26 +9,28 @@ import android.widget.ImageView
 class ImageView constructor(ctx: Context) : ImageView(ctx) {
 
     public var bitmap: Bitmap? = null
-    set(value){
-        field = value
-        this.setImageBitmap(field)
-        this.layoutParams.width = 100
-        this.layoutParams.height = 100
-        field?.let {
-            this.layoutParams.width = it.width
-            this.layoutParams.height = it.height
+
+        set(value){
+            field = value
+            this.setImageBitmap(field)
+            this.layoutParams.width = 100
+            this.layoutParams.height = 100
+            field?.let {
+                this.layoutParams.width = it.width
+                this.layoutParams.height = it.height
+            }
+            this.requestLayout()
         }
-        this.requestLayout()
-    }
 
     var position = PointF(0.0f, 0.0f)
-    set(value) {
-        field = value
-        val size = Size(width, height)
+        get() = PointF(this.x + width * 0.5f, this.y + height * 0.5f)
+        set(value) {
+            field = value
+            val size = Size(width, height)
 
-        this.x = field.x - size.width * 0.5f
-        this.y = field.y - size.height * 0.5f
-    }
+            this.x = field.x - size.width * 0.5f
+            this.y = field.y - size.height * 0.5f
+        }
 
 
 }
