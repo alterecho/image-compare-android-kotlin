0f132c25e1d3f6bf0c04db35f4e853f9742821c2
vj <alterechoxii@gmail.com>
2018-05-11 21:25:58 +0530

    added custom ImageView

---
+++ app/src/main/java/vjc/com/imagecompare/ImageView.kt
@@ -0,0 +1,34 @@
+package vjc.com.imagecompare
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.PointF
+import android.util.Size
+import android.widget.ImageView
+
+class ImageView constructor(ctx: Context) : ImageView(ctx) {
+
+    public var bitmap: Bitmap? = null
+    set(value){
+        field = value
+        this.setImageBitmap(field)
+        this.layoutParams.width = 100
+        this.layoutParams.height = 100
+        field?.let {
+            this.layoutParams.width = it.width
+            this.layoutParams.height = it.height
+        }
+        this.requestLayout()
+    }
+
+    var position = PointF(0.0f, 0.0f)
+    set(value) {
+        field = value
+        val size = Size(width, height)
+
+        this.x = field.x - size.width * 0.5f
+        this.y = field.y - size.height * 0.5f
+    }
+
+
+}
