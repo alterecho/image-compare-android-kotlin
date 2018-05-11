0fd40e21323540df486c08a689de72edb3904b15
vj <alterechoxii@gmail.com>
2018-05-11 23:13:21 +0530

    _imageView move for pointer1

--- app/src/main/java/com/vjc/imagecompare/view/ImageInspectorView.kt
+++ app/src/main/java/com/vjc/imagecompare/view/ImageInspectorView.kt
@@ -8,6 +8,7 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.widget.FrameLayout
 import com.vjc.imagecompare.extensions.*
+import com.vjc.imagecompare.model.Pointer
 
 class ImageInspectorView : FrameLayout {
 
@@ -31,28 +32,39 @@ class ImageInspectorView : FrameLayout {
         event?.let {
             val touchPoint = PointF(it.x, it.y);
 
+            val pointer = Pointer(event.actionIndex, event.getPointerId(event.actionIndex))
+
             when (it.action) {
                 MotionEvent.ACTION_DOWN -> {
                     println("down")
                     _touchPoint_began = touchPoint
                     _touchPoint_offset = touchPoint.bySubtracting(_imageView.position)
+
+                    _pointer1 = pointer
+
                     return true
                 }
 
                 MotionEvent.ACTION_POINTER_DOWN -> {
-
+                    _pointer2 = pointer
                 }
 
                 MotionEvent.ACTION_MOVE -> {
-                    _imageView.position = touchPoint.bySubtracting(_touchPoint_offset)
+                    if (pointer.equals(_pointer1)) {
+                        _imageView.position = touchPoint.bySubtracting(_touchPoint_offset)
+                    } else {
+
+                    }
+
                 }
 
                 MotionEvent.ACTION_POINTER_UP -> {
-
+                    _pointer2 = null
                 }
 
                 MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
                     _touchPoint_offset = PointF()
+                    _pointer1 = null
                 }
             }
         }
@@ -67,6 +79,9 @@ class ImageInspectorView : FrameLayout {
     private var _touchPoint_began = PointF(0.0f, 0.0f)
     /** difference between where the the touch began and where _imageView position (center) */
     private var _touchPoint_offset = PointF()
+
+    private var _pointer1: Pointer? = null
+    private var _pointer2: Pointer? = null
 
     private fun init() {
         setBackgroundColor(Color.CYAN)
