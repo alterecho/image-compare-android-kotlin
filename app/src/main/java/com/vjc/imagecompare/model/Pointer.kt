7d12bcc4117b13667fe33840f3a660fbffc8d1e8
vj <alterechoxii@gmail.com>
2018-05-12 12:31:19 +0530

    added secondary initializer for DataEvent

--- app/src/main/java/com/vjc/imagecompare/model/Pointer.kt
+++ app/src/main/java/com/vjc/imagecompare/model/Pointer.kt
@@ -1,10 +1,17 @@
 package com.vjc.imagecompare.model
 
+import android.view.MotionEvent
+
 data class Pointer constructor(val index: Int, val id: Int) {
+
+    constructor(event: MotionEvent) : this(event.actionIndex, event.getPointerId(event.actionIndex))
+
     override fun equals(other: Any?): Boolean {
         (other as? Pointer)?.let {
             return  other.id == this.id
         }
         return false
     }
+
+    override fun toString(): String = "id: $id, index: $index"
 }
