9b94bb9c8bc167d7d817683a8c7c0c6810d23a20
vj <alterechoxii@gmail.com>
2018-05-12 12:38:06 +0530

    implemented rotation

--- app/src/main/java/com/vjc/imagecompare/model/Pointer.kt
+++ app/src/main/java/com/vjc/imagecompare/model/Pointer.kt
@@ -1,10 +1,14 @@
 package com.vjc.imagecompare.model
 
+import android.graphics.Point
+import android.graphics.PointF
 import android.view.MotionEvent
 
 data class Pointer constructor(val index: Int, val id: Int) {
 
     constructor(event: MotionEvent) : this(event.actionIndex, event.getPointerId(event.actionIndex))
+
+    fun point(event: MotionEvent): PointF = PointF(event.getX(index), event.getY(index))
 
     override fun equals(other: Any?): Boolean {
         (other as? Pointer)?.let {
--- app/src/main/java/com/vjc/imagecompare/view/ImageInspectorView.kt
+++ app/src/main/java/com/vjc/imagecompare/view/ImageInspectorView.kt
@@ -9,6 +9,7 @@ import android.view.MotionEvent
 import android.widget.FrameLayout
 import com.vjc.imagecompare.extensions.*
 import com.vjc.imagecompare.model.Pointer
+import kotlin.math.atan2
 
 class ImageInspectorView : FrameLayout {
 
@@ -30,15 +31,22 @@ class ImageInspectorView : FrameLayout {
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         event?.let {
-            val touchPoint = PointF(it.x, it.y);
+            val p = PointF(it.x, it.y);
+            var p2 = PointF()
+            var angle = 0.0f
+            val pointer2 = _pointer2
+            if (pointer2 != null) {
+                p2 = pointer2.point(event)
+                angle = atan2(p2.y - _imageView.position.y, p2.x - _imageView.position.x)
+            }
 
-            val pointer = Pointer(event.actionIndex, event.getPointerId(event.actionIndex))
+            val pointer = Pointer(it)
 
-            when (it.action) {
+            when (it.actionMasked) {
                 MotionEvent.ACTION_DOWN -> {
                     println("down")
-                    _touchPoint_began = touchPoint
-                    _touchPoint_offset = touchPoint.bySubtracting(_imageView.position)
+                    _touchPoint_down = p
+                    _touchPoint_offset = p.bySubtracting(_imageView.position)
 
                     _pointer1 = pointer
 
@@ -47,13 +55,21 @@ class ImageInspectorView : FrameLayout {
 
                 MotionEvent.ACTION_POINTER_DOWN -> {
                     _pointer2 = pointer
+                    _startAngle_imageView = _imageView.rotation
+                    _startAngle = angle
+
+                    return true
                 }
 
                 MotionEvent.ACTION_MOVE -> {
-                    if (pointer.equals(_pointer1)) {
-                        _imageView.position = touchPoint.bySubtracting(_touchPoint_offset)
+                    if (pointer.id == pointer.id) {
+                        _imageView.position = p.bySubtracting(_touchPoint_offset)
                     } else {
 
+                    }
+
+                    if (pointer2 != null) {
+                        _imageView.rotation = _startAngle_imageView + Math.toDegrees((angle - _startAngle).toDouble()).toFloat();
                     }
 
                 }
@@ -76,12 +92,19 @@ class ImageInspectorView : FrameLayout {
 
 
     private val _imageView: ImageView = ImageView(this.context)
-    private var _touchPoint_began = PointF(0.0f, 0.0f)
+    private var _touchPoint_down = PointF(0.0f, 0.0f)
     /** difference between where the the touch began and where _imageView position (center) */
     private var _touchPoint_offset = PointF()
 
+    /** touch identifier for first and second touch (to differentiate movement and rotation) */
     private var _pointer1: Pointer? = null
     private var _pointer2: Pointer? = null
+
+    /** the angle w.r.t _imageView position (center), when the rotation starts */
+    private var _startAngle = 0.0f
+
+    /** the angle of the _imageView when thr rotation starts */
+    private var _startAngle_imageView = 0.0f
 
     private fun init() {
         setBackgroundColor(Color.CYAN)
